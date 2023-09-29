2c8945218a7c6d3bc67a01138cc334209cd86c71
demo <bajantrivijayalaxmi0@gmail.com>
2023-09-29 10:22:25 +0530

    h1 is added

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1> welcome to GPT</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
